585d3d73b5966a54835e824ade21ce6b660b84e4
Axel Davy <axel.davy@ens.fr>
2015-08-11 21:15:26 +0200

    f

--- lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -131,8 +131,8 @@ public:
     const AMDGPUSubtarget &ST = *getAMDGPUTargetMachine().getSubtargetImpl();
     if (ST.getGeneration() <= AMDGPUSubtarget::NORTHERN_ISLANDS)
       return createR600MachineScheduler(C);
-    //else //(uncomment to turn default for SI)
-    //  return createSIMachineScheduler(C);
+    else //(uncomment to turn default for SI)
+      return createSIMachineScheduler(C);
     return nullptr;
   }
 
